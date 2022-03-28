7ff4f91f354ba521f41b249d4f7dcc57fe43b3cd
“willy <[willykaria777@gmail.com]>
2022-03-28 09:11:44 +0300

    add a new component added store

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,122 +1,95 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex"
-          target="_blank"
-          rel="noopener"
-          >vuex</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+    <div class="left">
+      <h1>{{ title }}</h1>
+      <form @submit.prevent="addlink">
+        <input
+          type="text"
+          placeholder="Add a Link"
+          class="link-input"
+          v-model="newlink"
+        />
+      </form>
+      <ul>
+        <li v-for="(link, index) in links" :key="index">
+          {{ link }}
+        </li>
+      </ul>
+    </div>
+    <div class="right">
+      <stats />
+    </div>
   </div>
 </template>
 
 <script>
+import stats from "../components/stats.vue";
+import { mapState, mapMutations } from "vuex";
 export default {
   name: "HelloWorld",
-  props: {
-    msg: String,
+  data() {
+    return {
+      newlink: "",
+    };
+  },
+  components: { stats },
+  computed: mapState(["title", "links"]),
+  methods: {
+    ...mapMutations(["ADD_LINK"]),
+    addlink: function () {
+      this.ADD_LINK(this.newlink);
+      this.newlink = "";
+    },
   },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+html,
+#app,
+.home {
+  height: 100%;
 }
+body {
+  background-color: #f4f4f4;
+  margin: 0;
+  height: 100%;
+}
+
+.hello {
+  display: grid;
+  grid-template-columns: repeat(2, 50%);
+  grid-template-rows: 100%;
+  grid-template-areas: "left right";
+  height: 100%;
+}
+
+.left,
+.right {
+  padding: 30px;
+}
+
 ul {
   list-style-type: none;
   padding: 0;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+ul li {
+  padding: 20px;
+  background: white;
+  margin-bottom: 8px;
 }
-a {
-  color: #42b983;
+
+.right {
+  grid-area: right;
+  background-color: #e9e9e9;
+}
+input {
+  border: none;
+  padding: 20px;
+  width: calc(100% - 40px);
+  box-shadow: 0 5px 5px lightgrey;
+  margin-bottom: 50px;
+  outline: none;
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -4,9 +4,20 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  getters: {},
-  mutations: {},
+  state: {
+    title: "my custom title",
+    links: ["http://google.com", "http://coursetro.com", "http://youtube.com"],
+  },
+  getters: {
+    countLinks: (state) => {
+      return state.links.length;
+    },
+  },
+  mutations: {
+    ADD_LINK: (state, link) => {
+      state.links.push(link);
+    },
+  },
   actions: {},
   modules: {},
 });
